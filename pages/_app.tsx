3ee2cb44576e288b3a5564118f84f41ee16331ee
mikeyb <mike@mikes.email>
2022-11-01 18:51:54 -0700

    refactor: change to  nextjs v12 & update index/layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+import '../styles/normalize.css';
+import '../styles/skeleton.css';
 import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
